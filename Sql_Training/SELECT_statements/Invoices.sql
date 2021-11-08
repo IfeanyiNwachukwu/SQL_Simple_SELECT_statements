87a88f0ae68103dc36606af84468918419cfede1
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2021-11-08 02:57:21 +0100

    committing setup

---
+++ Sql_Training/SELECT_statements/Invoices.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Invoices](
+	[InvoiceID] [int] IDENTITY(1,1) NOT NULL,
+	[VendorID] [int] NOT NULL,
+	[InvoiceNumber] [varchar](50) NOT NULL,
+	[InvoiceDate] [date] NOT NULL,
+	[InvoiceTotal] [money] NOT NULL,
+	[PaymentTotal] [money] NOT NULL,
+	[CreditTotal] [money] NOT NULL,
+	[TermsID] [int] NOT NULL,
+	[InvoiceDueDate] [date] NOT NULL,
+	[PaymentDate] [date] NULL,
+ );
